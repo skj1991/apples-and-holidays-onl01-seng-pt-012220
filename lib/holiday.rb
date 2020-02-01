560ab679b3b7ab4b2b8701d2e29f7ae1b7b00e7b
Stacy Johns <stacykjohns@icloud.com>
2020-02-01 00:48:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
-    binding.pry
+    if season == :winter 
+      
+    end
   end
 end
 
