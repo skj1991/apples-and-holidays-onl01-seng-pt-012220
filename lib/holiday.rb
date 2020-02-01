b664bb03f78a71997c5dfd220fcee2ef001e808d
Stacy Johns <stacykjohns@icloud.com>
2020-02-01 01:19:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 winter_supplies = holiday_hash[winter]
+binding.pry
 end
 
 def all_supplies_in_holidays(holiday_hash)
